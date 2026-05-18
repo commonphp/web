1eb8f33fe75453153907a4e16af2aedfcfac4242
Timothy McClatchey <timothy@kansatech.com>
2026-05-17 20:06:28 -0500

    0.3 alpha

--- src/WebSurface.php
+++ src/WebSurface.php
@@ -5,8 +5,19 @@ declare(strict_types=1);
 namespace CommonPHP\Web;
 
 use CommonPHP\HTTP\Contracts\HttpSurfaceInterface;
+use CommonPHP\HTTP\Request;
+use CommonPHP\HTTP\Response;
+use CommonPHP\HTTP\ResponseFactory;
 
 class WebSurface implements HttpSurfaceInterface
 {
+    public function supports(Request $request): bool
+    {
+        return true;
+    }
 
+    public function handle(Request $request): Response
+    {
+        return (new ResponseFactory())->html('<h1>Not Found</h1>', 404);
+    }
 }
